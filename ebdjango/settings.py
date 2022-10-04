85a5eaf326dcff1581a8647562e2865c60e89c9f
AB047 <90478753+am11449@users.noreply.github.com>
2022-10-04 12:26:25 -0400

    Removed Secret key

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'swz@ea=cw*#wp-wq!ovv%vjweewmzm9w)gh8(&imr@17dri86a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
